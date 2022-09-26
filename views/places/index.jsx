8a4ad4cd8141f353d5909147a0f366cf6b46a62c
jeffdonkey <8jefa8@gmail.com>
2022-09-26 17:58:54 -0400

    places.js error correction

---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require ('react')
+const Def = require ('../default')
+
+function index () {
+    return (
+        <Def>
+            <main>
+                <h1>Index Stub</h1>
+                
+            </main>
+        </Def>
+    )
+    
+}
+
+module.exports = index
