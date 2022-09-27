7ab8267a1b00c16cb0854e161d4b208be850ae01
jeffdonkey <8jefa8@gmail.com>
2022-09-26 20:29:01 -0400

    adding styles

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//Modules and Globals
 require('dotenv').config()
 const express = require('express')
-
 const app = express()
 
+//Express Settings
+//NOTE: THE NEXT LINE BELOW WAS NOT IN THE INSTRUCTIONS FOR PART 1 OF THIS PROJECT
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', function (req, res){
@@ -13,5 +18,5 @@ app.get('/', function (req, res){
 })
 
 
-
+// Listen for Connections
 app.listen(process.env.PORT)
--- views/home.jsx
+++ views/home.jsx
@@ -1,13 +1,16 @@
 //importing React and Def (from default.jsx)
-const React = require ('react')
-const Def = require ('./default')
+const React = require('react')
+const Def = require('./default')
 
 //another stub function
-function home () {
+function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>                
+                <h1>HOME</h1>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
     )
