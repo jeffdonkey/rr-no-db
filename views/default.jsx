20f799ca8c192907b987af43b8a73cd898343a26
jeffdonkey <8jefa8@gmail.com>
2022-09-28 19:32:05 -0400

    Navbar added

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,29 @@
 // imports "React"
 const React = require('react')
 
-//Stub function/Method stub
-//this is a piece of code used to stand in for some other programming functionality.
-//so can be a placeholder or used for testing
-function Def (html) {
+
+function Def(html) {
     return (
         <html>
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
-                <link rel="stylesheet" href="/css/style.css"/>
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <nav>
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+        <a href="/places">Places</a>
+    </li>
+    <li>
+        <a href="/places/new">Add Place</a>
+    </li>
+</ul>
+                </nav>
                 {html.children}
             </body>
         </html>
